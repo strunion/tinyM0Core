27e3224d35356bafc6d03e0744a5de6c8a8e429a
Zerg17 <zerg1703@yandex.ru>
2023-06-11 18:22:30 +0300

    Add proc1 and proc2 realization.

--- src/main.c
+++ src/main.c
@@ -1,7 +1,32 @@
 #include "stm32f0xx.h"
 #include "tinyM0Core.h"
 
+void proc1(void){
+    while(1){
+            for(int i = 0; i < 20; i++){
+            GPIOA->BSRR |= GPIO_BSRR_BR_5;
+            GPIOA->BSRR |= GPIO_BSRR_BS_5;
+        }
+        yield();
+    }
+}
+
+void proc2(void){
+    while(1){
+        for(int i = 0; i < 20; i++){
+            GPIOA->BSRR |= GPIO_BSRR_BR_6;
+            GPIOA->BSRR |= GPIO_BSRR_BS_6;
+        }
+        yield();
+    }
+}
+
 int main(void){
+    RCC->AHBENR |= RCC_AHBENR_GPIOAEN | RCC_AHBENR_GPIOBEN;
+    GPIOA->MODER |= GPIO_MODER_MODER6_1 | GPIO_MODER_MODER5_0;
+
+    osStart();
+
     while(1){
 
     }
